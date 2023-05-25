b62e2254bb41e3fd5442505e98a5230920c8c879
Nihat Aliyev <naliyev04.44@gmail.com>
2023-05-25 16:36:49 +0400

    new portfolio, sidebar

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="row"></div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/LeftSidebar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+// images
+import Logo from "../assets/images/logo.png";
+import Me from "../assets/images/me.jpg";
+import { Link } from "react-router-dom";
+// icons
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+import { AiOutlineSetting } from "react-icons/ai";
+
+export const LeftSidebar = () => {
+  return (
+    <div className="left-sidebar">
+      <span className="setting">
+        <AiOutlineSetting className="setting-icon" />
+      </span>
+      <div className="sidebar-header">
+        <div className="sidebar-logo">
+          <img className="sidebar-logo-img" src={Logo} alt="logo" />
+        </div>
+        <div className="sidebar-job">
+          <p className="sidebar-job-text">Framer Designer & Developer</p>
+        </div>
+      </div>
+      <div className="sidebar-main">
+        <img className="sidebar-main-img" src={Me} alt="me" />
+        <h2 className="gmail">hello@drake.design</h2>
+        <h2 className="country">Based in Los Angeles, CA</h2>
+        <p className="copyright">© 2022 Drake. All Rights Reserved</p>
+        <div className="social">
+          <Link className="social-link">
+            <AiOutlineInstagram />
+          </Link>
+          <Link className="social-link">
+            <AiOutlineTwitter />
+          </Link>
+          <Link className="social-link">
+            <AiFillLinkedin />
+          </Link>
+          <Link className="social-link">
+            <AiFillGithub />
+          </Link>
+        </div>
+        <Link className="sidebar-btn">hire me!</Link>
+      </div>
+    </div>
+  );
+};
