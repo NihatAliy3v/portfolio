1951a3c5a2cb098096df2419b36a6be54b789733
Nihat Aliyev <naliyev04.44@gmail.com>
2023-06-23 17:50:59 +0400

    create hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineHome } from "react-icons/ai";
 
 export const Hero = () => {
   return (
-    <section className='hero'>
-        <div className="container">
-            <div className="row">sdfsdf</div>
+    <section className="hero">
+      <div className="container">
+        <div className="row">
+          <div className="section-head">
+            <AiOutlineHome className="icon-element" />
+            <h2 className="title">introduce</h2>
+          </div>
+          <div className="hero-main">
+            <h1 className="hero-title">
+              Say Hi from <span className="name">Drake</span>, Webflow Designer and Developer
+            </h1>
+            <p className="hero-summary">
+              I design and code beautifully simple things and i love what i do.
+              Just simple like that!
+            </p>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
