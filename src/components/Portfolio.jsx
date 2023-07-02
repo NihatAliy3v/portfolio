6a9e4c10dbad02d00bb329ca34879ead641ed8ce
Nihat Aliyev <naliyev04.44@gmail.com>
2023-07-02 23:52:03 +0400

    fixed: portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { IoShapesOutline } from "react-icons/io5";
+import portfolio1 from "../assets/images/portfolio1.jpg";
+import portfolio2 from "../assets/images/portfolio2.jpg";
+import portfolio3 from "../assets/images/portfolio3.jpg";
+import portfolio4 from "../assets/images/portfolio4.jpg";
 
 export const Portfolio = () => {
   return (
@@ -15,12 +19,66 @@ export const Portfolio = () => {
             <h2 className="title">portfolio</h2>
           </div>
           <h2
-            className="porfolio-title"
+            className="portfolio-title"
             data-aos="fade-up"
             data-aos-duration="1500"
           >
             Featured <span className="strong">Projects</span>
           </h2>
+          <div className="portfolio-main">
+            <div className="card-group">
+              <div className="card-line">
+                <div
+                  className="card"
+                  data-aos="fade-up"
+                  data-aos-duration="1500"
+                >
+                  <div className="image-item">
+                    <img src={portfolio1} alt="" />
+                  </div>
+                  <h4 className="title">
+                    Bureau - Architecture Studio Website
+                  </h4>
+                </div>
+              </div>
+              <div className="card-line">
+                <div
+                  className="card"
+                  data-aos="fade-right"
+                  data-aos-duration="1500"
+                >
+                  <div className="image-item">
+                    <img src={portfolio2} alt="" />
+                  </div>
+                  <h4 className="title">Moonex WordPress Theme</h4>
+                </div>
+                <div
+                  className="card"
+                  data-aos="fade-left"
+                  data-aos-duration="1500"
+                >
+                  <div className="image-item">
+                    <img src={portfolio3} alt="" />
+                  </div>
+                  <h4 className="title">Taskly Dashboard</h4>
+                </div>
+              </div>
+              <div className="card-line">
+                <div
+                  className="card"
+                  data-aos="fade-up"
+                  data-aos-duration="1500"
+                >
+                  <div className="image-item">
+                    <img src={portfolio4} alt="" />
+                  </div>
+                  <h4 className="title">
+                    Hinterland - Real Estate Site Redesign
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
