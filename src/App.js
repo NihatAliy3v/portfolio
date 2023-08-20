f5dc1bbaf85950686fbf5f5c7e2bbd900938be39
Nihat Aliyev <naliyev04.44@gmail.com>
2023-08-20 22:15:25 +0400

    fix build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Header } from "./components/Header";
 import Home from "./pages/Home";
-import { useEffect, useState } from "react";
 
 function App() {
 
